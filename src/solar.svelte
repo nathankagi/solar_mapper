c67143202c3fd6cd829cc2b1523f16c829e42784
Nathan <npkagi@outlook.com>
2024-01-16 20:49:17 +0800

    cleaning

--- src/solar.svelte
+++ src/solar.svelte
@@ -3,26 +3,7 @@
 </script>
 
 <main>
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <title>Solar Mapper</title>
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="stylesheet" type="text/css" href="index.css" />
-    </head>
-
-    <body>
-      <script type="module" src="./index.ts"></script>
-    </body>
-  </html>
 </main>
 
 <style>
-  html,
-  body {
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-  }
 </style>
